6f593daf413d64bace82cdd3c2e41cc5cafd9d2d
radnov <radnov@pm.me>
2021-06-10 17:23:48 +0300

    DEVOPS-57 Add correct paths and filenames

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -3,35 +3,30 @@
 set -e
 
 declare -a SOURCES
-declare -r MAIN_DIR="complete"
-declare -r OPTIONAL_DIR="dashboard"
-
-# TODO
-# construct destination
-# have correct locale subdirs
+declare -r MAIN_DIR="test/complete"
+declare -r SUBSET_DIR="test/dashboard"
 
 function getUploadables {
-  FILES=($(ls "$MAIN_DIR"))
-  SOURCES=( "${FILES[@]/#/$MAIN_DIR/}" )
+  SOURCES=($(find "$MAIN_DIR" -type f | sort))
 
-  if [ -d "$OPTIONAL_DIR" ]; then
-    OPTIONAL_FILES=($(ls "$OPTIONAL_DIR"))
-    SOURCES+=( "${OPTIONAL_FILES[@]/#/$OPTIONAL_DIR/}" )
+  if [ -d "$SUBSET_DIR" ]; then
+    subset=($(find "$SUBSET_DIR" -type f | sort))
+    SOURCES+=("${subset[@]}")
   fi
 }
 
-# if the extension is json it's a "package"
+# if the extension is json, it's a "package"
 function isPackage {
   [[ "${1#*.}" == "json" ]]
 }
 
-# if the extension is html or xlsx it's a "reference"
+# if the extension is html or xlsx, it's a "reference"
 function isReference {
   [[ "${1#*.}" == "html" ]] || [[ "${1#*.}" == "xlsx" ]]
 }
 
-# get JSON object "package"
-function getPackageVersion {
+# get "package" JSON object from file
+function getPackageObject {
   if isPackage "$1"; then
     jq -r '.package' < "$1"
   fi
@@ -42,48 +37,76 @@ function createJson {
   jq -n --arg key "$1" --arg value "$2" '[{"source": $key, "destination": $value}]'
 }
 
-# append $2 to $1
+# append JSON $2 to $1
 function addToJson {
   echo "$1" | jq -c --argjson new "$2" '. += $new'
 }
 
-function createDestination {
-  # incomplete
-  DESTINATION=''
-  if [[ $1 =~ $OPTIONAL_DIR  ]]; then
-    DESTINATION+="$OPTIONAL_DIR/"
+# create path from "package" JSON object
+function createPath {
+  # get package details from object
+  locale=$(echo "$1" | jq -r '.locale')
+  code=$(echo "$1" | jq -r '.code')
+  type=$(echo "$1" | jq -r '.type')
+  package_version=$(echo "$1" | jq -r '.version')
+  dhis2_version=$(echo "$1" | jq -r '.DHIS2Version')
+
+  path="$locale/$code/$type/$package_version/$dhis2_version"
+
+  if [[ $1 =~ $SUBSET_DIR  ]]; then
+    path+="$SUBSET_DIR/"
   fi
+
+  echo "$path"
 }
 
-function createMatrix {
-  matrix='[]'
-
-  # create source -> destination for all files
+# get reference path and file name from the first "package" found
+function getReferenceDestination {
   files=("$@")
+
   for file in "${files[@]}"
   do
-    createDestination "$file"
-    packageVersion=$(getPackageVersion "$file")
-    echo "$packageVersion"
+    object=$(getPackageObject "$file")
 
-    # default filename for references?
-    FILE_NAME=$(echo "$packageVersion" | jq -r '.name')
+    # remove locale from path
+    path=$(createPath "$object")
+    path_without_locale=${path#*/}
+
+    # remove locale from name
+    name=$(echo "$object" | jq -r '.name')
+    name_without_locale=${name%-*}
+
+    echo "$path_without_locale/$name_without_locale"
+
+    # return after first found file
+    return
+  done
+}
+
+# create matrix of sources and destinations
+function createMatrix {
+  files=("$@")
+
+  matrix='[]'
+
+  reference_destination=$(getReferenceDestination "${files[@]}")
+
+  # create source -> destination for all files
+  for file in "${files[@]}"
+  do
+    packageObject=$(getPackageObject "$file")
+    path=$(createPath "$packageObject")
+
+    file_name=$(echo "$packageObject" | jq -r '.name')
 
     if isPackage "$file"; then
-      # TODO Remove this! if there is _ in the name, it's a translation
-      if [[ "${file%%.*}" =~ "_" ]]; then
-        filename="${file%%.*}"
-        locale="${filename#*_}"
-        # replace the default "en" locale
-        addition=$(createJson "$file" "$DESTINATION${FILE_NAME%-*}-$locale.${file#*.}")
-      else
-        addition=$(createJson "$file" "$DESTINATION$FILE_NAME.${file#*.}")
-      fi
+      addition=$(createJson "$file" "$path/$file_name.${file#*.}")
       matrix=$(addToJson "$matrix" "$addition")
     fi
 
     if isReference "$file"; then
-      addition=$(createJson "$file" "$DESTINATION$FILE_NAME-ref.${file#*.}")
+      locale=$(basename $(dirname "$file"))
+      addition=$(createJson "$file" "$locale/$reference_destination-$locale-ref.${file#*.}")
       matrix=$(addToJson "$matrix" "$addition")
     fi
   done
